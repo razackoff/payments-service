7fd3b4904e16157e70392fa6099b77528809264b
firuz-macbook <razackoff@gmail.com>
2024-03-20 15:00:55 +0200

    Add data base connection and migration.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using hotel_booking_service.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string dbHost = Environment.GetEnvironmentVariable("DATABASE_HOST")!;
+string dbName = Environment.GetEnvironmentVariable("DATABASE_NAME")!;
+string dbUser = Environment.GetEnvironmentVariable("DATABASE_USER")!;
+string dbPassword = Environment.GetEnvironmentVariable("DATABASE_PASSWORD")!;
+
+string connectionString = $"Host={dbHost};Database={dbName};Username={dbUser};Password={dbPassword};";
+//string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,29 +33,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
